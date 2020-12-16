fe5cdc5013ebab74ef489241f296305057b1fe7b
Ahtee <benottedev@gmail.com>
2020-12-16 15:55:40 -0500

    Initial commit, vue app

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Logo :alt="logoAltText" :src="logo" />
+    <Intro :title="title" :headline="headline" />
+  </div>
+</template>
+
+<script>
+import Intro from "./components/Intro.vue";
+import Logo from "./components/Logo.vue";
+
+import face from "./assets/face.jpg";
+
+export default {
+  components: {
+    Intro,
+    Logo,
+  },
+  data() {
+    return {
+      logoAltText: "Ben Otte",
+      logo: face,
+      title: "Ben Otte",
+      headline: "Software Engineer, Cloud certified",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="intro-section">
+    <h1>{{ title }}</h1>
+    <h2>{{ headline }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    headline: String,
+  },
+};
+</script>
